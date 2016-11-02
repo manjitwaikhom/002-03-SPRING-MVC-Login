7205830e993153a02124f56cccf5c82830f6f1d1
manjitwaikhom <manjitwaikhom@gmail.com>
2016-11-02 11:31:37 +0530

    logger added

--- 002-03-SPRING MVC-Login(XML)/src/main/java/com/spring/mvc/LoginController.java
+++ 002-03-SPRING MVC-Login(XML)/src/main/java/com/spring/mvc/LoginController.java
@@ -3,31 +3,37 @@ package com.spring.mvc;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
-
+import org.apache.log4j.Logger;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
+public class LoginController extends SimpleFormController {
+	final static Logger LOGGER = Logger.getLogger(LoginController.class);
 
-public class LoginController extends SimpleFormController{
 	/**********************************************************/
-	public ModelAndView onSubmit(Object command,BindException errors)throws ServletException{
+	public ModelAndView onSubmit(Object command, BindException errors)
+			throws ServletException {
+		LOGGER.info("onSubmit() method called");
 		System.out.println("inside onSumbit() method");
-		User user=(User)command;
-		String un=user.getUsername();
-		String pw=user.getPassword();
-		String view="";
-		if(un.equals(pw)){
-			view=getSuccessView();
-		}else{
-			view=getFormView();
+		User user = (User) command;
+		String un = user.getUsername();
+		String pw = user.getPassword();
+		String view = "";
+		if (un.equals(pw)) {
+			view = getSuccessView();
+		} else {
+			view = getFormView();
 		}
-		return new ModelAndView(view,"user",user);
+		return new ModelAndView(view, "user", user);
 	}
+
 	/*****************************************************/
-	public Object formBackingObject(HttpServletRequest request)throws ServletException{
+	public Object formBackingObject(HttpServletRequest request)
+			throws ServletException {
+		LOGGER.info("formBackingObject() method called");
 		System.out.println("inside formBackingObject() method");
-		User user=new User();
+		User user = new User();
 		user.setUsername("manjit");
 		return user;
 	}
